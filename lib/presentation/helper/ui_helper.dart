3e204d07cc5961155a66a90d69fae84d62aa7074
avijit155 <avijitgoswami72@gmail.com>
2025-06-30 02:54:48 +0530

    Task-1: chore: constants added

---
+++ lib/presentation/helper/ui_helper.dart
@@ -0,0 +1,8 @@
+extension StringCasingExtension on String {
+  String capitalizeEachWord() {
+    return split(' ')
+        .map((word) =>
+    word.isNotEmpty ? '${word[0].toUpperCase()}${word.substring(1)}' : '')
+        .join(' ');
+  }
+}
